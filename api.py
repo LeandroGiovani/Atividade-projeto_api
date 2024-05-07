0334ca7e48ce6bb62cf740e553f3ae4031c933e4
Leandro Giovani <127615704+LeandroGiovani@users.noreply.github.com>
2024-05-07 10:55:25 -0300

    Integrando com html

--- api.py
+++ api.py
@@ -4,36 +4,36 @@ from random_data import *
 
 bp = Blueprint("api", __name__)
 
-@bp.route('/api/', methods=("GET"))
+@bp.route('/api', methods=("GET",))
 def index():
     return jsonify({"status": 200, "message": "API do Leandro Maciel Giovani"})
 
-@bp.route('/api/aleatorio', methods=("GET"))
-def aleatorio():
+@bp.route('/api/aleatorios', methods=("GET",))
+def aleatorios():
     import random
     a = random.randint(49, 100)
     return jsonify({"status": 200, "data": a})
 
-@bp.route('/api/argumentos/<string:nome>', methods=("GET"))
+@bp.route('/api/argumentos/<string:nome>', methods=("GET",))
 def argumentos(nome: str):
     return jsonify({"status": 200, "data": nome})
 
-@bp.route('/api/maiores_de_50', methods=("GET"))
-def A():
+@bp.route('/api/idades', methods=("GET",))
+def idades():
     return jsonify(f'Quantidade: {maior_de_50(pessoas)}')
 
-@bp.route('/api/ganham_mais_de_2000', methods=("GET"))
-def B():
+@bp.route('/api/salarios', methods=("GET",))
+def salarios():
     return jsonify(f'Ganham mais de 2000R$: {mais_2000(pessoas)[0]} pessoas, representando {mais_2000(pessoas)[1]}% de {mais_2000(pessoas)[2]} pessoas no total')
 
-@bp.route('/api/3_maiores_salarios', methods=("GET"))
-def C():
-    return jsonify(f'{maior2(pessoas)}', methods=("GET"))
+@bp.route('/api/maioressalarios', methods=("GET",))
+def maioressalarios():
+    return jsonify(f'{maior2(pessoas)}')
 
-@bp.route('/api/media_salarial_profissoes', methods=("GET"))
-def D():
+@bp.route('/api/profissoes', methods=("GET",))
+def profissoes():
     return jsonify(media_profissoes(pessoas))
 
-@bp.route('/api/intervalo_idades2000', methods=("GET"))
-def E():
+@bp.route('/api/intervalo', methods=("GET",))
+def intervalo():
     return jsonify(maior_2000_sexo(pessoas, sexo='Masculino'), maior_2000_sexo(pessoas, sexo='Feminino'))
--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=("GET",))
 def index():
     return render_template("index.html")
 
