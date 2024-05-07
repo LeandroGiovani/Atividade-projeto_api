682f30f35cff3b67c3392b27ab9185b64a7ab0f4
Leandro Giovani <127615704+LeandroGiovani@users.noreply.github.com>
2024-05-07 08:13:45 -0300

    rotas index e main

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 from api import bp
 app.register_blueprint(bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
