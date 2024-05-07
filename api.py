1d8f6015824a65de827d484828f43f694f34cbc2
Leandro Giovani <127615704+LeandroGiovani@users.noreply.github.com>
2024-05-07 08:05:43 -0300

    registrando blueprint

--- api.py
+++ api.py
@@ -1,39 +1,39 @@
 from flask import Blueprint, jsonify, request
-import random
 from funcoes import *
 from random_data import *
 
 bp = Blueprint("api", __name__)
 
-@bp.route('/api/')
+@bp.route('/api/', methods=("GET"))
 def index():
     return jsonify({"status": 200, "message": "API do Leandro Maciel Giovani"})
 
-@bp.route('/api/aleatorio')
+@bp.route('/api/aleatorio', methods=("GET"))
 def aleatorio():
+    import random
     a = random.randint(49, 100)
     return jsonify({"status": 200, "data": a})
 
-@bp.route('/api/argumentos/<string:nome>')
+@bp.route('/api/argumentos/<string:nome>', methods=("GET"))
 def argumentos(nome: str):
     return jsonify({"status": 200, "data": nome})
 
-@bp.route('/api/maiores_de_50')
+@bp.route('/api/maiores_de_50', methods=("GET"))
 def A():
     return jsonify(f'Quantidade: {maior_de_50(pessoas)}')
 
-@bp.route('/api/ganham_mais_de_2000')
+@bp.route('/api/ganham_mais_de_2000', methods=("GET"))
 def B():
     return jsonify(f'Ganham mais de 2000R$: {mais_2000(pessoas)[0]} pessoas, representando {mais_2000(pessoas)[1]}% de {mais_2000(pessoas)[2]} pessoas no total')
 
-@bp.route('/api/3_maiores_salarios')
+@bp.route('/api/3_maiores_salarios', methods=("GET"))
 def C():
-    return jsonify(f'{maior2(pessoas)}')
+    return jsonify(f'{maior2(pessoas)}', methods=("GET"))
 
-@bp.route('/api/media_salarial_profissoes')
+@bp.route('/api/media_salarial_profissoes', methods=("GET"))
 def D():
     return jsonify(media_profissoes(pessoas))
 
-@bp.route('/api/intervalo_idades2000')
+@bp.route('/api/intervalo_idades2000', methods=("GET"))
 def E():
     return jsonify(maior_2000_sexo(pessoas, sexo='Masculino'), maior_2000_sexo(pessoas, sexo='Feminino'))
--- app.py
+++ app.py
@@ -1,49 +1,9 @@
-from flask import Flask, jsonify
-from funcoes import *
-from random_data import *
-import random
-
-# A - Pessoas maiores de 50 anos
-# B - Pessoas que ganham mais de R$ 2000 e a porcentagem; que isso representa do todo;
-# C - O nome, salário, idade e profissão das 3 pessoas com maiores salários
-# D - A média salarial de cada profissão
-# E - O  intervalo da maioria das idades e o sexo de quem ganha mais de R$ 2000. (M 39-50, F 29-30)
-# --------------------------------------------------------
-# Z - Sair
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return jsonify({"status": 200, "message": "API do Leandro Maciel Giovani"})
+from api import bp
+app.register_blueprint(bp)
 
-@app.route('/aleatorio')
-def aleatorio():
-    a = random.randint(49, 100)
-    return jsonify({"status": 200, "data": a})
-
-@app.route('/argumentos/<string:nome>')
-def argumentos(nome: str):
-    return jsonify({"status": 200, "data": nome})
-
-@app.route('/maiores_de_50')
-def A():
-    return jsonify(f'Quantidade: {maior_de_50(pessoas)}')
-
-@app.route('/ganham_mais_de_2000')
-def B():
-    return jsonify(f'Ganham mais de 2000R$: {mais_2000(pessoas)[0]} pessoas, representando {mais_2000(pessoas)[1]}% de {mais_2000(pessoas)[2]} pessoas no total')
-
-@app.route('/3_maiores_salarios')
-def C():
-    return jsonify(f'{maior2(pessoas)}')
-
-@app.route('/media_salarial_profissoes')
-def D():
-    return jsonify(media_profissoes(pessoas))
-
-@app.route('/intervalo_idades2000')
-def E():
-    return jsonify(maior_2000_sexo(pessoas, sexo='Masculino'), maior_2000_sexo(pessoas, sexo='Feminino'))
 if __name__ == '__main__':
     app.run(debug=True)
